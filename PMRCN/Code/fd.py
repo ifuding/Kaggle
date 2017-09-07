c4610290636600f6757593dfa600e4cdbcd593ee
ifuding <fu_ding1991@qq.com>
2017-09-07 08:20:12 -0700

    Intermediate data

--- PMRCN/Code/fd.py
+++ PMRCN/Code/fd.py
@@ -6,7 +6,7 @@ import numpy as np
 import xgboost as xgb
 import lightgbm as lgb
 
-full_feature = False
+full_feature = True
 
 data_folder = '../Data/'
 train = pd.read_csv(data_folder + 'training_variants')
@@ -25,79 +25,88 @@ train = train.drop(['Class'], axis=1)
 test = pd.merge(test, testx, how='left', on='ID').fillna('')
 pid = test['ID'].values
 
-df_all = pd.concat((train, test), axis=0, ignore_index=True)
-df_all['Gene_Share'] = df_all.apply(lambda r: sum([1 for w in r['Gene'].split(' ') if w in r['Text'].split(' ')]), axis=1).astype(np.int8)
-df_all['Variation_Share'] = df_all.apply(lambda r: sum([1 for w in r['Variation'].split(' ') if w in r['Text'].split(' ')]), axis=1).astype(np.int8)
-
-print df_all[['Gene_Share', 'Variation_Share']].max()
-# exit(0)
-if full_feature:
-    #commented for Kaggle Limits
-    for i in range(56):
-        df_all['Gene_'+str(i)] = df_all['Gene'].map(lambda x: str(x[i]) if len(x)>i else '')
-        df_all['Variation'+str(i)] = df_all['Variation'].map(lambda x: str(x[i]) if len(x)>i else '')
-    print df_all.dtypes
-
-    gen_var_lst = sorted(list(train.Gene.unique()) + list(train.Variation.unique()))
-    print(len(gen_var_lst))
-    gen_var_lst = [x for x in gen_var_lst if len(x.split(' '))==1]
-    print(len(gen_var_lst))
-    i_ = 0
-    #commented for Kaggle Limits
-    for gen_var_lst_itm in gen_var_lst:
-        if i_ % 100 == 0: print(i_)
-        df_all['GV_'+str(gen_var_lst_itm)] = df_all['Text'].map(lambda x: str(x).count(str(gen_var_lst_itm))).astype(np.int8)
-        i_ += 1
-
-for c in df_all.columns:
-    if df_all[c].dtype == 'object':
-        if c in ['Gene','Variation']:
-            lbl = preprocessing.LabelEncoder()
-            df_all[c+'_lbl_enc'] = lbl.fit_transform(df_all[c].values)
-            df_all[c+'_len'] = df_all[c].map(lambda x: len(str(x)))
-            df_all[c+'_words'] = df_all[c].map(lambda x: len(str(x).split(' ')))
-        elif c != 'Text':
-            lbl = preprocessing.LabelEncoder()
-            df_all[c] = lbl.fit_transform(df_all[c].values)
-        if c=='Text':
-            df_all[c+'_len'] = df_all[c].map(lambda x: len(str(x)))
-            df_all[c+'_words'] = df_all[c].map(lambda x: len(str(x).split(' ')))
-
-train = df_all.iloc[:len(train)]
-print train.dtypes
-test = df_all.iloc[len(train):]
-
-class cust_regression_vals(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
-    def fit(self, x, y=None):
-        return self
-    def transform(self, x):
-        x = x.drop(['Gene', 'Variation','ID','Text'],axis=1).values
-        return x
-
-class cust_txt_col(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
-    def __init__(self, key):
-        self.key = key
-    def fit(self, x, y=None):
-        return self
-    def transform(self, x):
-        return x[self.key].apply(str)
-
-print('Pipeline...')
-fp = pipeline.Pipeline([
-    ('union', pipeline.FeatureUnion(
-        n_jobs = -1,
-        transformer_list = [
-            ('standard', cust_regression_vals()),
-            ('pi1', pipeline.Pipeline([('Gene', cust_txt_col('Gene')), ('count_Gene', feature_extraction.text.CountVectorizer(analyzer=u'char', ngram_range=(1, 8))), ('tsvd1', decomposition.TruncatedSVD(n_components=20, n_iter=25, random_state=12))])),
-            ('pi2', pipeline.Pipeline([('Variation', cust_txt_col('Variation')), ('count_Variation', feature_extraction.text.CountVectorizer(analyzer=u'char', ngram_range=(1, 8))), ('tsvd2', decomposition.TruncatedSVD(n_components=20, n_iter=25, random_state=12))])),
-            #commented for Kaggle Limits
-            ('pi3', pipeline.Pipeline([('Text', cust_txt_col('Text')), ('tfidf_Text', feature_extraction.text.TfidfVectorizer(ngram_range=(1, 2))), ('tsvd3', decomposition.TruncatedSVD(n_components=50, n_iter=25, random_state=12))]))
-        ])
-    )])
-
-train = fp.fit_transform(train); print(train.shape)
-test = fp.transform(test); print(test.shape)
-
+#df_all = pd.concat((train, test), axis=0, ignore_index=True)
+#df_all['Gene_Share'] = df_all.apply(lambda r: sum([1 for w in r['Gene'].split(' ') if w in r['Text'].split(' ')]), axis=1).astype(np.int8)
+#df_all['Variation_Share'] = df_all.apply(lambda r: sum([1 for w in r['Variation'].split(' ') if w in r['Text'].split(' ')]), axis=1).astype(np.int8)
+#
+#print df_all[['Gene_Share', 'Variation_Share']].max()
+## exit(0)
+#if full_feature:
+#    #commented for Kaggle Limits
+#    for i in range(56):
+#        df_all['Gene_'+str(i)] = df_all['Gene'].map(lambda x: str(x[i]) if len(x)>i else '')
+#        df_all['Variation'+str(i)] = df_all['Variation'].map(lambda x: str(x[i]) if len(x)>i else '')
+#    print df_all.dtypes
+#
+#    gen_var_lst = sorted(list(train.Gene.unique()) + list(train.Variation.unique()))
+#    print(len(gen_var_lst))
+#    gen_var_lst = [x for x in gen_var_lst if len(x.split(' '))==1]
+#    print(len(gen_var_lst))
+#    i_ = 0
+#    #commented for Kaggle Limits
+#    for gen_var_lst_itm in gen_var_lst:
+#        if i_ % 100 == 0: print(i_)
+#        df_all['GV_'+str(gen_var_lst_itm)] = df_all['Text'].map(lambda x: str(x).count(str(gen_var_lst_itm))).astype(np.int8)
+#        i_ += 1
+#
+#for c in df_all.columns:
+#    if df_all[c].dtype == 'object':
+#        if c in ['Gene','Variation']:
+#            lbl = preprocessing.LabelEncoder()
+#            df_all[c+'_lbl_enc'] = lbl.fit_transform(df_all[c].values)
+#            df_all[c+'_len'] = df_all[c].map(lambda x: len(str(x)))
+#            df_all[c+'_words'] = df_all[c].map(lambda x: len(str(x).split(' ')))
+#        elif c != 'Text':
+#            lbl = preprocessing.LabelEncoder()
+#            df_all[c] = lbl.fit_transform(df_all[c].values)
+#        if c=='Text':
+#            df_all[c+'_len'] = df_all[c].map(lambda x: len(str(x)))
+#            df_all[c+'_words'] = df_all[c].map(lambda x: len(str(x).split(' ')))
+#
+#train = df_all.iloc[:len(train)]
+#print train.dtypes
+#test = df_all.iloc[len(train):]
+#
+#class cust_regression_vals(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+#    def fit(self, x, y=None):
+#        return self
+#    def transform(self, x):
+#        x = x.drop(['Gene', 'Variation','ID','Text'],axis=1).values
+#        return x
+#
+#class cust_txt_col(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+#    def __init__(self, key):
+#        self.key = key
+#    def fit(self, x, y=None):
+#        return self
+#    def transform(self, x):
+#        return x[self.key].apply(str)
+#
+#print('Pipeline...')
+#fp = pipeline.Pipeline([
+#    ('union', pipeline.FeatureUnion(
+#        n_jobs = -1,
+#        transformer_list = [
+#            ('standard', cust_regression_vals()),
+#            ('pi1', pipeline.Pipeline([('Gene', cust_txt_col('Gene')), ('count_Gene', feature_extraction.text.CountVectorizer(analyzer=u'char', ngram_range=(1, 8))), ('tsvd1', decomposition.TruncatedSVD(n_components=20, n_iter=25, random_state=12))])),
+#            ('pi2', pipeline.Pipeline([('Variation', cust_txt_col('Variation')), ('count_Variation', feature_extraction.text.CountVectorizer(analyzer=u'char', ngram_range=(1, 8))), ('tsvd2', decomposition.TruncatedSVD(n_components=20, n_iter=25, random_state=12))])),
+#            #commented for Kaggle Limits
+#            ('pi3', pipeline.Pipeline([('Text', cust_txt_col('Text')), ('tfidf_Text', feature_extraction.text.TfidfVectorizer(ngram_range=(1, 2))), ('tsvd3', decomposition.TruncatedSVD(n_components=50, n_iter=25, random_state=12))]))
+#        ])
+#    )])
+#
+#train = fp.fit_transform(train);
+#print type(train)
+#print(train.shape)
+#print (train.nbytes)
+#np.save("train_array", train)
+## print(df.dtypes)
+## print(df.memory_usage())
+#test = fp.transform(test); print(test.shape)
+#np.save("test_array", test)
+#exit(0)
+train = np.load("./train_array.npy")
+test = np.load("./test_array.npy")
 y = y - 1 #fix for zero bound array
 
 def xgbTrain(flod = 5):
@@ -170,7 +179,7 @@ def lgbm_train(model_k = None):
         'feature_fraction': 1,
         'verbose': 1,
       #   'valid_sets': [d_valide],
-        'num_boost_round': 13
+        'num_boost_round': 50
     }
 
     # ROUNDS = 1
@@ -210,5 +219,6 @@ def gen_sub(models, model_type):
 if __name__ == "__main__":
     # model_k, th, F1 = keras_train(10)
     # gen_sub(model_k, 'k', th, F1)
-    model_l = lgbm_train()#model_k)
-    gen_sub(model_l, 'l') #model_k)
+    xgbTrain();
+    # model_l = lgbm_train()#model_k)
+    # gen_sub(model_l, 'l') #model_k)
