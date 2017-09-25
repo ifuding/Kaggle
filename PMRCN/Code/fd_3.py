217df86b3bb44424afc7da2b3b3b1737f155bf9f
ifuding <fu_ding1991@qq.com>
2017-09-26 01:23:45 +0800

    linear combine

--- PMRCN/Code/fd_3.py
+++ PMRCN/Code/fd_3.py
@@ -28,9 +28,10 @@ from sklearn.metrics import log_loss
 from keras import __version__ as keras_version
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras import regularizers
 
 # DNN_PARAMS
-HIDDEN_UNITS = [64, 64, 32]
+HIDDEN_UNITS = [40, 20, 4]
 DNN_EPOCHS = 40
 BATCH_SIZE = 5
 DNN_BN = True
@@ -189,7 +190,7 @@ lgbm_train_label = y[siamese_train_len:]
 #test = test[:200]
 #pid = pid[:200]
 
-def xgbTrain(train_data, train_label, fold = 5):
+def xgbTrain(train_data, train_label, fold = 5, valide_data = None, valide_label = None):
     """
     """
     denom = 0
@@ -205,7 +206,10 @@ def xgbTrain(train_data, train_label, fold = 5):
             'seed': i,
             'silent': True
         }
-        x1, x2, y1, y2 = model_selection.train_test_split(train_data, train_label, test_size=0.18, random_state=i)
+        if valide_data is None:
+            x1, x2, y1, y2 = model_selection.train_test_split(train_data, train_label, test_size=0.18, random_state=i)
+        else:
+            x1, x2, y1, y2 = train_data, valide_data, train_label, valide_label
         watchlist = [(xgb.DMatrix(x1, y1), 'train'), (xgb.DMatrix(x2, y2), 'valid')]
         model = xgb.train(params, xgb.DMatrix(x1, y1), 1000,  watchlist, verbose_eval=50, early_stopping_rounds=100)
         score1 = metrics.log_loss(y2, model.predict(xgb.DMatrix(x2), ntree_limit=model.best_ntree_limit), labels = list(range(9)))
@@ -275,7 +279,7 @@ def lgbm_train(train_data, train_label, fold = 5, valide_data = None, valide_lab
                         )
         #cv_result = lgb.cv(params, d_train, nfold=10)
         #pd.DataFrame(cv_result).to_csv('cv_result', index = False)
-        exit(0)
+        # exit(0)
         models.append((bst, 'l'))
 
     return models
@@ -288,11 +292,11 @@ def create_model(input_len):
         model.add(BatchNormalization())
     if DROPOUT_RATE > 0:
         model.add(Dropout(DROPOUT_RATE))
-    model.add(Dense(HIDDEN_UNITS[2], activation='sigmoid'))
-    if DNN_BN:
-        model.add(BatchNormalization())
-    if DROPOUT_RATE > 0:
-        model.add(Dropout(DROPOUT_RATE))
+   # model.add(Dense(HIDDEN_UNITS[2], activation='sigmoid'))
+   # if DNN_BN:
+   #     model.add(BatchNormalization())
+   # if DROPOUT_RATE > 0:
+   #     model.add(Dropout(DROPOUT_RATE))
    # model.add(Dense(HIDDEN_UNITS[2], activation='sigmoid'))
    # if DNN_BN:
    #     model.add(BatchNormalization())
@@ -312,7 +316,8 @@ def create_model(input_len):
 
 def create_lr_model(input_len):
     model = Sequential()
-    model.add(Dense(9, activation='softmax', input_dim = input_len))
+    model.add(Dense(9, activation='softmax', input_dim = input_len, kernel_regularizer=regularizers.l2(0.01),
+                        activity_regularizer=regularizers.l1(0.01)))
 
     # optimizer = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     # optimizer = RMSprop(lr=1e-3, rho = 0.9, epsilon = 1e-8)
@@ -372,10 +377,6 @@ def keras_train(train_data, train_label, nfolds = 10, valide_data = None, valide
     models = []
     for train_index, test_index in kf:
         # model = create_model(classes = 2)
-        if model_type == 'DNN':
-            model = create_embedding_model(len(CONTINUOUS_INDICES), len(SPARSE_INDICES))
-        else model_type == 'LR':
-            model = create_lr_model()
         # model = create_siamese_net((train.shape)[1])
 
         if valide_data is None:
@@ -393,6 +394,7 @@ def keras_train(train_data, train_label, nfolds = 10, valide_data = None, valide
         else:
             X_valid = valide_data
             Y_valid = valide_label
+
         print('Positive samples in valide: %d' % np.sum(Y_valid))
         print('Negative samples in valide: %d' % (len(Y_valid) - np.sum(Y_valid)))
 
@@ -401,13 +403,23 @@ def keras_train(train_data, train_label, nfolds = 10, valide_data = None, valide
         print('Split train: ', len(X_train), len(Y_train))
         print('Split valid: ', len(X_valid), len(Y_valid))
 
+        if model_type == 'DNN':
+            model = create_embedding_model(len(CONTINUOUS_INDICES), len(SPARSE_INDICES))
+            X_train = gen_dnn_input(X_train)
+            X_valid = gen_dnn_input(X_valid)
+        elif model_type == 'LR':
+            model = create_lr_model(X_train.shape[1])
+        else:
+            print('unknown keras model')
+            exit(1)
+
         callbacks = [
             EarlyStopping(monitor='val_loss', patience=3, verbose=0),
         ]
-        model.fit(gen_dnn_input(X_train),
+        model.fit(X_train,
                 Y_train, batch_size=BATCH_SIZE, epochs=DNN_EPOCHS,
                 shuffle=True, verbose=2,
-                validation_data=(gen_dnn_input(X_valid), Y_valid)
+                validation_data=(X_valid, Y_valid)
                 , callbacks=callbacks)
 
         model_name = 'keras' + strftime('_%Y_%m_%d_%H_%M_%S', gmtime())
@@ -533,15 +545,61 @@ def gen_sub(models, merge_features):
     submission.to_csv(sub_name, index=False)
 
 
+#def linear_combine(preds_array, preds, labels, remain_linear_sum, linear_array, min_loss, ind, min_linear_array):
+#    """
+#    """
+#    if (len(preds_array) == ind):
+#        loss = metrics.logloss(labels, preds)
+#        if min_loss > loss:
+#            min_linear_array = linear_array.copy()
+#        return
+#    if ind == len(preds_array) - 1:
+#        linear_array[ind] = remain_linear_sum
+#        preds += remain_linear_sum * preds_array[ind]
+#        remain_linear_sum = 0
+#    else:
+#        for ll in range(0, remain_linear_sum * 10 + 1):
+#            ll *= 0.1
+#            linear_array[ind] = ll
+#            preds += ll * preds_array[ind]
+
+def linear_combine(preds_array, labels):
+    """
+    """
+    min_loss = 10000
+    opt_l = 0
+    opt_r = 0
+    for l in range(0, 11):
+        l *= 0.1
+        r = 1 - l
+        preds = l * preds_array[0] + r * preds_array[1]
+        loss = metrics.log_loss(labels, preds)
+        if loss < min_loss:
+            min_loss = loss
+            opt_l = l
+            opt_r = r
+            print('min_loss: {} opt_l: {} opt_r: {}'.format(min_loss, opt_l, opt_r))
+    # print('min_loss: {} opt_l: {} opt_r: {}'.format(min_loss, opt_l, opt_r))
+    return opt_l, opt_r
+
+
 if __name__ == "__main__":
     #rnn_input = gen_rnn_input(train_text, MAX_NUM_WORDS, MAX_SEQUENCE_LEN)
     #model_k = rnn_train(rnn_input, y, 10)
     #exit(0)
-    model_k = keras_train(TRAIN_DATA, TRAIN_LABEL, 10, VALIDE_DATA, VALIDE_LABEL)
-    keras_preds_train = models_eval(model_k, gen_dnn_input(train))
-    np.save("keras_preds" + \
-            strftime('_%Y_%m_%d_%H_%M_%S', gmtime()) , keras_preds_train)
+    #model_k = keras_train(TRAIN_DATA, TRAIN_LABEL, 2, VALIDE_DATA, VALIDE_LABEL, 'DNN')
+    #keras_preds_train = models_eval(model_k, gen_dnn_input(train))
+    #np.save("keras_preds" + \
+    #        strftime('_%Y_%m_%d_%H_%M_%S', gmtime()) , keras_preds_train)
     # keras_preds_train = np.load('keras_preds_2017_09_24_16_28_23.npy')
+    model_l = lgbm_train(TRAIN_DATA, TRAIN_LABEL, 2, VALIDE_DATA, VALIDE_LABEL)
+    lgbm_preds_train = models_eval(model_l, train)
+    model_x = xgbTrain(TRAIN_DATA, TRAIN_LABEL, 2, VALIDE_DATA, VALIDE_LABEL)
+    xgb_preds_train = models_eval(model_x, train)
+    # merge_train_data = np.concatenate((keras_preds_train, lgbm_preds_train), axis = 1)
+    linear_combine(np.array([xgb_preds_train[TEST_INDEX], lgbm_preds_train[TEST_INDEX]]), VALIDE_LABEL)
+    # model_k = keras_train(merge_train_data[TRAIN_INDEX], TRAIN_LABEL, 2, merge_train_data[TEST_INDEX], VALIDE_LABEL, 'LR')
+    exit(0)
     print(train.shape)
     print(keras_preds_train.shape)
     merge_train_data = np.concatenate((train, keras_preds_train), axis = 1)
