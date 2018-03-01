11aa1e96d15045b7c658924e21b091170c59d176
ifuding <fu_ding1991@qq.com>
2018-03-02 00:04:58 +0800

    Set maxfeature in TFIDF

--- TCCC/Code/ToxicAvenger.py
+++ TCCC/Code/ToxicAvenger.py
@@ -4,6 +4,7 @@ import sklearn
 from sklearn import feature_extraction, ensemble, decomposition, pipeline
 from textblob import TextBlob
 from nfold_train import nfold_train, models_eval
+from time import gmtime, strftime
 
 zpolarity = {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',10:'ten'}
 zsign = {-1:'negative',  0.: 'neutral', 1:'positive'}
@@ -27,8 +28,8 @@ tid = test['id'].values
 # train['comment_text'] = train.apply(lambda r: str(r['comment_text']) + ' polarity' +  zsign[np.sign(r['polarity'])] + zpolarity[np.abs(r['polarity'])], axis=1)
 # test['comment_text'] = test.apply(lambda r: str(r['comment_text']) + ' polarity' +  zsign[np.sign(r['polarity'])] + zpolarity[np.abs(r['polarity'])], axis=1)
 
-# df = pd.concat([train['comment_text'], test['comment_text']], axis=0)
-# df = df.fillna("unknown")
+df = pd.concat([train['comment_text'], test['comment_text']], axis=0)
+df = df.fillna("unknown")
 
 # class cust_txt_col(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
 #    def __init__(self, key):
@@ -38,19 +39,23 @@ tid = test['id'].values
 #    def transform(self, x):
 #        return x[self.key].apply(str)
 
-# print('Pipeline...')
-# fp = pipeline.Pipeline([
-#    ('union', pipeline.FeatureUnion(
-#        n_jobs = -1,
-#        transformer_list = [
-#            ('pi1', pipeline.Pipeline([('count_comment_text', feature_extraction.text.CountVectorizer(analyzer=u'char', ngram_range=(1, 8))), \
-#                 ('tsvd1', decomposition.TruncatedSVD(n_components=20, n_iter=1, random_state=12))])),
-#            # ('pi2', pipeline.Pipeline([('tfidf_Text', feature_extraction.text.TfidfVectorizer(ngram_range=(1, 2))), \
-#            #     ('tsvd2', decomposition.TruncatedSVD(n_components=50, n_iter=25, random_state=12))]))
-#        ])
-#    )])
+print('Pipeline...')
+fp = pipeline.Pipeline([
+   ('union', pipeline.FeatureUnion(
+       n_jobs = -1,
+       transformer_list = [
+           ('pi1', pipeline.Pipeline([('count_comment_text', \
+                feature_extraction.text.TfidfVectorizer(stop_words='english', analyzer=u'char', ngram_range=(2, 4), max_features=5000)), \
+                ('tsvd1', \
+                decomposition.TruncatedSVD(n_components=20, n_iter=1, random_state=12))])),
+           ('pi2', pipeline.Pipeline([('tfidf_Text', \
+                feature_extraction.text.TfidfVectorizer(stop_words='english', ngram_range=(1, 2), max_features=5000)), \
+                ('tsvd2', \
+                decomposition.TruncatedSVD(n_components=50, n_iter=25, random_state=12))]))
+       ])
+   )])
 
-# data = fp.fit_transform(df[:1000]);
+data = fp.fit_transform(df)
 # exit(0)
 # tfidf = feature_extraction.text.TfidfVectorizer(stop_words='english', ngram_range=(1, 2), max_features=800000)
 # svd = decomposition.TruncatedSVD(n_components=50, n_iter=25, random_state=12)
@@ -58,8 +63,9 @@ tid = test['id'].values
 # data = tfidf.fit_transform(df)
 # print("SVD------")
 # data = svd.fit_transform(data)
-# np.save('svd.npy', data)
-data = np.load('svd.npy')
+svd_name = "svd" + strftime('_%Y_%m_%d_%H_%M_%S', gmtime()) + ".npy"
+np.save(svd_name, data)
+# data = np.load('svd.npy')
 train_data, train_label = data[:nrow], y
 test_data = data[nrow:]
 
@@ -69,7 +75,7 @@ sub2 = pd.DataFrame(np.zeros((test.shape[0], len(coly))), columns = coly)
 for c in coly:
     print("------Label: {0}".format(c))
     label = train_label[c].values
-    models, _, _, _ = nfold_train(train_data, label, fold = 5, model_types = 'k') #, valide_label = train_label)
+    models, _, _, _ = nfold_train(train_data, label, fold = 5, model_types = 'l') #, valide_label = train_label)
     multi_label_models.append(models)
     sub2[c] = models_eval(models, test_data)
 #model = ensemble.ExtraTreesClassifier(n_jobs=-1, random_state=3)
